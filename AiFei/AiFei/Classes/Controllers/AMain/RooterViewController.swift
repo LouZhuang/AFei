0898301e664ae4a79932e148af300bfa32cc9aa7
HuangLc <3283180700@qq.com>
2016-04-18 21:24:28 +0800

    整体框架搭建

---
+++ AiFei/AiFei/Classes/Controllers/AMain/RooterViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RooterViewController.swift
+//  AiFei
+//
+//  Created by 黄礼潮 on 16/4/18.
+//  Copyright © 2016年 黄礼潮. All rights reserved.
+//
+
+import UIKit
+
+class RooterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.yellowColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
---
+++ AiFei/AiFei/Classes/Controllers/IPlay/IPlayViewController.swift
@@ -0,0 +1,26 @@
+//
+//  IPlayViewController.swift
+//  AiFei
+//
+//  Created by 黄礼潮 on 16/4/18.
+//  Copyright © 2016年 黄礼潮. All rights reserved.
+//
+
+import UIKit
+
+class IPlayViewController: RooterViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+}
